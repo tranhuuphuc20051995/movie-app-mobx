ee84d7459cea9a96179faffa0de104e95d4d81a1
Phuc Tran Huu <phuc.tranh@one-line.com>
2022-08-02 10:05:12 +0700

    Create project structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:movie_app/main.dart';
+import 'package:movie_app/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
