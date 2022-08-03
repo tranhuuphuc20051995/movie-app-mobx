77b45b833231db5dbb70d055cb33f2ca329f8f2d
Phuc Tran Huu <phuc.tranh@one-line.com>
2022-08-03 15:50:47 +0700

    Add request params for functions of movie remote data source

--- lib/data/datasources/remote/movie_remote_data_source.dart
+++ lib/data/datasources/remote/movie_remote_data_source.dart
@@ -1,15 +1,17 @@
 import 'package:movie_app/data/models/movie_top_rated_response.dart';
 import 'package:movie_app/data/models/video_response.dart';
 
+import '../../../domain/model/request_params/base_request_param.dart';
+import '../../../domain/model/request_params/movie_request_param.dart';
 import '../../models/movie_detail_response.dart';
 import '../../api_services/movie_service.dart';
 
 abstract class MovieRemoteDataSource {
-  Future<MovieDetailResponse> getMovieDetail(int id);
+  Future<MovieDetailResponse> getMovieDetail(MovieDetailRequestParam params);
 
-  Future<VideoResponse> getVideosOfMovie(int id);
+  Future<VideoResponse> getVideosOfMovie(MovieDetailRequestParam params);
 
-  Future<MovieTopRatedResponse> getTopRated();
+  Future<MovieTopRatedResponse> getTopRated(BaseRequestParam params);
 }
 
 class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
@@ -18,17 +20,28 @@ class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
   MovieClientService movieClientService;
 
   @override
-  Future<MovieTopRatedResponse> getTopRated() {
-    return movieClientService.getTopRated();
+  Future<MovieTopRatedResponse> getTopRated(BaseRequestParam params) {
+    return movieClientService.getTopRated(
+      params.apiKey,
+      params.language,
+    );
   }
 
   @override
-  Future<MovieDetailResponse> getMovieDetail(int id) {
-    return movieClientService.getMovieDetail(id);
+  Future<MovieDetailResponse> getMovieDetail(MovieDetailRequestParam params) {
+    return movieClientService.getMovieDetail(
+      params.apiKey,
+      params.language,
+      params.movieId,
+    );
   }
 
   @override
-  Future<VideoResponse> getVideosOfMovie(int id) {
-    return movieClientService.getVideosOfMovie(id);
+  Future<VideoResponse> getVideosOfMovie(MovieDetailRequestParam params) {
+    return movieClientService.getVideosOfMovie(
+      params.apiKey,
+      params.language,
+      params.movieId,
+    );
   }
 }
