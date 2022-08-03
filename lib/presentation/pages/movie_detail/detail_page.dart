841ca6385916c0228b905d560674a8687d3ff7a4
Phuc Tran Huu <phuc.tranh@one-line.com>
2022-08-03 23:22:27 +0700

    Update UI for detail page & fix issue load backdrop image

--- lib/presentation/pages/movie_detail/detail_page.dart
+++ lib/presentation/pages/movie_detail/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import '../../../app/routes/app_routes.dart';
@@ -25,58 +26,72 @@ class MovieDetailPage extends StatelessWidget {
       ),
       body: Observer(
         builder: (BuildContext context) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(
-                'https://image.tmdb.org/t/p/w500${_movieDetailPageController.movieDetail?.posterPath}',
-                loadingBuilder: (
-                  context,
-                  child,
-                  loadingProgress,
-                ) =>
-                    const CircularProgressIndicator(),
-                errorBuilder: (context, url, error) => const Icon(Icons.error),
-              ),
-              Text(
-                _movieDetailPageController.movieDetail?.title ?? '',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                _movieDetailPageController.movieDetail?.releaseDate ?? '',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                _movieDetailPageController.movieDetail?.overview ?? '',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8),
-              GestureDetector(
-                onTap: () async {
-                  await _movieDetailPageController.onPlayTrailer(args.movieId);
-                  if (_movieDetailPageController.videos?.videos?.isEmpty ==
-                      true) {
-                    return;
-                  }
-                  final List<String> trailerLinks = [];
-                  _movieDetailPageController.videos?.videos?.forEach((video) {
-                    if (video.key?.isNotEmpty == true) {
-                      trailerLinks.add(video.key!);
-                    }
-                  });
-                  Navigator.of(context).pushNamed(
-                    AppRoute.movieTrailerPage,
-                    arguments: TrailerArguments(trailerLinks: trailerLinks),
-                  );
-                },
-                child: const Icon(
-                  Icons.play_arrow_rounded,
-                  size: 48,
+          if (_movieDetailPageController.movieDetail != null) {
+            return Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        'https://image.tmdb.org/t/p/w500${args.backdropImage}',
+                    fit: BoxFit.fitWidth,
+                    placeholder: (
+                      BuildContext context,
+                      String url,
+                    ) =>
+                        const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                Text(
+                  _movieDetailPageController.movieDetail?.title ?? '',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  _movieDetailPageController.movieDetail?.releaseDate ?? '',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  _movieDetailPageController.movieDetail?.overview ?? '',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                GestureDetector(
+                  onTap: () async {
+                    await _movieDetailPageController
+                        .onPlayTrailer(args.movieId);
+                    if (_movieDetailPageController.videos?.videos?.isEmpty ==
+                        true) {
+                      return;
+                    }
+                    final List<String> trailerLinks = [];
+                    _movieDetailPageController.videos?.videos?.forEach((video) {
+                      if (video.key?.isNotEmpty == true) {
+                        trailerLinks.add(video.key!);
+                      }
+                    });
+                    Navigator.of(context).pushNamed(
+                      AppRoute.movieTrailerPage,
+                      arguments: TrailerArguments(trailerLinks: trailerLinks),
+                    );
+                  },
+                  child: const Icon(
+                    Icons.play_arrow_rounded,
+                    size: 48,
+                  ),
+                ),
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
           );
         },
       ),
