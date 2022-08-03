0e887cf47d5bc36c878e21ad506f37b43793bd3a
Phuc Tran Huu <phuc.tranh@one-line.com>
2022-08-03 15:54:20 +0700

    Add logic init dependency injection by getIt

---
+++ lib/app/di/di_inject.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:flutter_pretty_dio_logger/flutter_pretty_dio_logger.dart';
+import '../../data/api_services/movie_service.dart';
+import '../../data/datasources/remote/movie_remote_data_source.dart';
+import '../../data/repository/movie_repository_impl.dart';
+import '../../domain/repository/movie_repository.dart';
+import '../../domain/usecase/movie/get_movie_detail_use_case.dart';
+import '../../domain/usecase/movie/get_movie_top_rated_use_case.dart';
+import '../../domain/usecase/movie/get_movie_videos_use_case.dart';
+import '../../presentation/controllers/home_page_controller.dart';
+
+GetIt locator = GetIt.instance;
+
+Future<void> initDI() async {
+  final dio = Dio();
+  dio.interceptors.add(
+    PrettyDioLogger(
+      requestHeader: true,
+      queryParameters: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+      error: true,
+      showProcessingTime: true,
+      // canShowLog: kDebugMode,
+    ),
+  );
+
+  locator
+    ..registerLazySingleton<Dio>(() => dio)
+    ..registerLazySingleton<MovieClientService>(
+        () => MovieClientService(locator()))
+    ..registerLazySingleton<MovieRemoteDataSource>(
+        () => MovieRemoteDataSourceImpl(locator()))
+    ..registerLazySingleton<MovieRepository>(
+        () => MovieRepositoryImpl(locator()))
+    ..registerLazySingleton<GetMovieDetailUseCase>(
+        () => GetMovieDetailUseCase(locator()))
+    ..registerLazySingleton<GetMovieTopRatedUseCase>(
+        () => GetMovieTopRatedUseCase(locator()))
+    ..registerLazySingleton<GetMovieVideosUseCase>(
+        () => GetMovieVideosUseCase(locator()))
+    ..registerLazySingleton<HomePageController>(
+        () => HomePageController(locator()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
+import 'app/di/di_inject.dart';
 
 void main() {
+  mainApp();
+}
+
+Future<void> mainApp() async {
+  await initDI();
   runApp(const MyApp());
 }
